d9ba43ef814c2472de9d9d8a703c6d0509a4605c
Nige <nigelbirkenshaw@gmail.com>
2018-06-28 18:43:53 -0400

    Structs for all gtfs basic types but needs validation for required fields and parsing elements which are not strings such as dates, booleans and enumerated types

--- gtfs.go
+++ gtfs.go
@@ -79,6 +79,119 @@ func (t *Trip) String() string {
 	return t.Id + " " + t.RouteId + " " + t.ServiceId
 }
 
+// From stop_times.txt
+type StopTime struct {
+	TripId            string `gtfs_name:"trip_id" gtfs_required:"true"`
+	ArrivalTime       string `gtfs_name:"arrival_time" gtfs_required:"true"`
+	DepartureTime     string `gtfs_name:"departure_time" gtfs_required:"true"`
+	StopId            string `gtfs_name:"stop_id" gtfs_required:"true"`
+	StopSequence      string `gtfs_name:"stop_sequence" gtfs_required:"true"`
+	StopHeadSign      string `gtfs_name:"stop_headsign" gtfs_required:"false"`
+	PickupType        string `gtfs_name:"pickup_type" gtfs_required:"false"`
+	DropOffType       string `gtfs_name:"drop_off_type" gtfs_required:"false"`
+	ShapeDistTraveled string `gtfs_name:"shape_dist_traveled" gtfs_required:"false"`
+	TimePoint         string `gtfs_name:"timepoint" gtfs_required:"false"`
+}
+
+func (st *StopTime) String() string {
+	return st.TripId + " " + st.StopId
+}
+
+// From calendar.txt
+type Service struct {
+	ServiceId string `gtfs_name:"service_id" gtfs_required:"true"`
+	Monday    string `gtfs_name:"monday" gtfs_required:"true"`
+	Tuesday   string `gtfs_name:"tuesday" gtfs_required:"true"`
+	Wednesday string `gtfs_name:"wednesday" gtfs_required:"true"`
+	Thursday  string `gtfs_name:"thursday" gtfs_required:"true"`
+	Friday    string `gtfs_name:"friday" gtfs_required:"true"`
+	Saturday  string `gtfs_name:"saturday" gtfs_required:"true"`
+	Sunday    string `gtfs_name:"sunday" gtfs_required:"true"`
+	StartDate string `gtfs_name:"start_date" gtfs_required:"true"`
+	EndDate   string `gtfs_name:"end_date" gtfs_required:"true"`
+}
+
+func (s *Service) String() string {
+	return s.ServiceId + " from " + s.StartDate + " to " + s.EndDate
+}
+
+// From calendar_dates.txt
+type ServiceException struct {
+	ServiceId     string `gtfs_name:"service_id" gtfs_required:"true"`
+	Date          string `gtfs_name:"date" gtfs_required:"true"`
+	ExceptionType string `gtfs_name:"exception_type" gtfs_required:"true"`
+}
+
+func (s *ServiceException) String() string {
+	return s.ServiceId + " on " + s.Date + " exception " + s.ExceptionType
+}
+
+// From fare_attributes.txt
+type Fare struct {
+	FareId           string `gtfs_name:"fare_id" gtfs_required:"true"`
+	Price            string `gtfs_name:"price" gtfs_required:"true"`
+	CurrencyType     string `gtfs_name:"currency_type" gtfs_required:"true"`
+	PaymentMethod    string `gtfs_name:"payment_method" gtfs_required:"true"`
+	Transfers        string `gtfs_name:"transfers" gtfs_required:"true"`
+	AgencyId         string `gtfs_name:"agency_id" gtfs_required:"false"`
+	TransferDuration string `gtfs_name:"transfer_duration" gtfs_required:"false"`
+}
+
+func (f *Fare) String() string {
+	return f.FareId + " " + f.Price + f.CurrencyType
+}
+
+// From fare_rules.txt
+type FareRule struct {
+	FareId        string `gtfs_name:"fare_id" gtfs_required:"true"`
+	RouteId       string `gtfs_name:"route_id" gtfs_required:"false"`
+	OriginId      string `gtfs_name:"origin_id" gtfs_required:"false"`
+	DestinationId string `gtfs_name:"destination_id" gtfs_required:"false"`
+	ContainsId    string `gtfs_name:"contains_id" gtfs_required:"false"`
+}
+
+func (f *FareRule) String() string {
+	return f.FareId + " " + f.RouteId + " " + f.OriginId + " " + f.DestinationId
+}
+
+// From shapes.txt
+type ShapePoint struct {
+	Id           string `gtfs_name:"shape_id" gtfs_required:"true"`
+	PtLatitude   string `gtfs_name:"shape_pt_lat" gtfs_required:"true"`
+	PtLongitude  string `gtfs_name:"shape_pt_lon" gtfs_required:"true"`
+	PtSequence   string `gtfs_name:"shape_pt_sequence" gtfs_required:"true"`
+	DistTraveled string `gtfs_name:"shape_dist_traveled" gtfs_required:"false"`
+}
+
+func (s *ShapePoint) String() string {
+	return s.Id + " " + s.PtSequence + " " + s.PtLatitude + " " + s.PtLongitude
+}
+
+// From frequencies.txt
+type Frequency struct {
+	TripId      string `gtfs_name:"trip_id" gtfs_required:"true"`
+	StartTime   string `gtfs_name:"start_time" gtfs_required:"true"`
+	EndTime     string `gtfs_name:"end_time" gtfs_required:"true"`
+	HeadwaySecs string `gtfs_name:"headway_secs" gtfs_required:"true"`
+	ExactTimes  string `gtfs_name:"exact_times" gtfs_required:"false"`
+}
+
+func (f *Frequency) String() string {
+	return f.TripId + " " + f.StartTime + " " + f.EndTime
+}
+
+// From transfers.txt
+type Transfer struct {
+	FromStopId          string `gtfs_name:"from_stop_id" gtfs_required:"true"`
+	ToStopId            string `gtfs_name:"to_stop_id" gtfs_required:"true"`
+	TransferType        string `gtfs_name:"transfer_type" gtfs_required:"true"`
+	MinimumTransferTime string `gtfs_name:"min_transfer_time" gtfs_required:"false"`
+}
+
+func (t *Transfer) String() string {
+	return t.FromStopId + " to " + t.ToStopId + " " + t.TransferType
+}
+
 func getFieldIndexForStruct(t reflect.Type, name string) (int, error) {
 	for i := 0; i < t.NumField(); i++ {
 		f := t.Field(i)
--- gtfs_test.go
+++ gtfs_test.go
@@ -84,3 +84,185 @@ A,WE,AWE2,Downtown,2`
 		}
 	}
 }
+func TestStopTimes(t *testing.T) {
+	s := `trip_id,arrival_time,departure_time,stop_id,stop_sequence,pickup_type,drop_off_type
+AWE1,0:06:10,0:06:10,S1,1,0,0
+AWE1,,,S2,2,1,3
+AWE1,0:06:20,0:06:30,S3,3,0,0
+AWE1,,,S5,4,0,0
+AWE1,0:06:45,0:06:45,S6,5,0,0
+AWD1,0:06:10,0:06:10,S1,1,0,0
+AWD1,,,S2,2,0,0
+AWD1,0:06:20,0:06:20,S3,3,0,0
+AWD1,,,S4,4,0,0
+AWD1,,,S5,5,0,0
+AWD1,0:06:45,0:06:45,S6,6,0,0`
+
+	out, err := Decode(strings.NewReader(s), &StopTime{})
+	if err != nil {
+		t.Error(err)
+	} else {
+		if len(out) == 0 {
+			t.Errorf("No output")
+		} else {
+			for i := 0; i < len(out); i++ {
+				t.Log(out[i].(*StopTime).String())
+			}
+			t.Errorf("Need to validate the struct")
+		}
+	}
+}
+
+func TestServices(t *testing.T) {
+	s := `service_id,monday,tuesday,wednesday,thursday,friday,saturday,sunday,start_date,end_date
+WE,0,0,0,0,0,1,1,20060701,20060731
+WD,1,1,1,1,1,0,0,20060701,20060731`
+
+	out, err := Decode(strings.NewReader(s), &Service{})
+	if err != nil {
+		t.Error(err)
+	} else {
+		if len(out) == 0 {
+			t.Errorf("No output")
+		} else {
+			for i := 0; i < len(out); i++ {
+				t.Log(out[i].(*Service).String())
+			}
+			t.Errorf("Need to validate the struct")
+		}
+	}
+}
+func TestServiceExceptions(t *testing.T) {
+	s := `service_id,date,exception_type
+WD,20060703,2
+WE,20060703,1
+WD,20060704,2
+WE,20060704,1`
+
+	out, err := Decode(strings.NewReader(s), &ServiceException{})
+	if err != nil {
+		t.Error(err)
+	} else {
+		if len(out) == 0 {
+			t.Errorf("No output")
+		} else {
+			for i := 0; i < len(out); i++ {
+				t.Log(out[i].(*ServiceException).String())
+			}
+			t.Errorf("Need to validate the struct")
+		}
+	}
+}
+func TestFares(t *testing.T) {
+	s := `fare_id,price,currency_type,payment_method,transfers,transfer_duration
+1,0.00,USD,0,0,0
+2,0.50,USD,0,0,0
+3,1.50,USD,0,0,0
+4,2.00,USD,0,0,0
+5,2.50,USD,0,0,0`
+
+	out, err := Decode(strings.NewReader(s), &Fare{})
+	if err != nil {
+		t.Error(err)
+	} else {
+		if len(out) == 0 {
+			t.Errorf("No output")
+		} else {
+			for i := 0; i < len(out); i++ {
+				t.Log(out[i].(*Fare).String())
+			}
+			t.Errorf("Need to validate the struct")
+		}
+	}
+}
+
+func TestFareRules(t *testing.T) {
+	s := `fare_id,route_id,origin_id,destination_id,contains_id
+a,TSW,1,1,
+a,TSE,1,1,
+a,GRT,1,1,
+a,GRJ,1,1,
+a,SVJ,1,1,
+a,JSV,1,1,
+a,GRT,2,4,
+a,GRJ,4,2,
+b,GRT,3,3,
+c,GRT,,,6`
+
+	out, err := Decode(strings.NewReader(s), &FareRule{})
+	if err != nil {
+		t.Error(err)
+	} else {
+		if len(out) == 0 {
+			t.Errorf("No output")
+		} else {
+			for i := 0; i < len(out); i++ {
+				t.Log(out[i].(*FareRule).String())
+			}
+			t.Errorf("Need to validate the struct")
+		}
+	}
+}
+
+func TestShapePoints(t *testing.T) {
+	s := `shape_id,shape_pt_lat,shape_pt_lon,shape_pt_sequence,shape_dist_traveled
+A_shp,37.61956,-122.48161,1,0
+A_shp,37.64430,-122.41070,2,6.8310
+A_shp,37.65863,-122.30839,3,15.8765`
+
+	out, err := Decode(strings.NewReader(s), &ShapePoint{})
+	if err != nil {
+		t.Error(err)
+	} else {
+		if len(out) == 0 {
+			t.Errorf("No output")
+		} else {
+			for i := 0; i < len(out); i++ {
+				t.Log(out[i].(*ShapePoint).String())
+			}
+			t.Errorf("Need to validate the struct")
+		}
+	}
+}
+
+func TestFrequencies(t *testing.T) {
+	s := `trip_id,start_time,end_time,headway_secs
+AWE1,05:30:00,06:30:00,300
+AWE1,06:30:00,20:30:00,180
+AWE1,20:30:00,28:00:00,420`
+
+	out, err := Decode(strings.NewReader(s), &Frequency{})
+	if err != nil {
+		t.Error(err)
+	} else {
+		if len(out) == 0 {
+			t.Errorf("No output")
+		} else {
+			for i := 0; i < len(out); i++ {
+				t.Log(out[i].(*Frequency).String())
+			}
+			t.Errorf("Need to validate the struct")
+		}
+	}
+}
+
+func TestTransfers(t *testing.T) {
+	s := `from_stop_id,to_stop_id,transfer_type,min_transfer_time
+S6,S7,2,300
+S7,S6,3,
+S23,S7,1,`
+
+	out, err := Decode(strings.NewReader(s), &Transfer{})
+	if err != nil {
+		t.Error(err)
+	} else {
+		if len(out) == 0 {
+			t.Errorf("No output")
+		} else {
+			for i := 0; i < len(out); i++ {
+				t.Log(out[i].(*Transfer).String())
+			}
+			t.Errorf("Need to validate the struct")
+		}
+	}
+}
